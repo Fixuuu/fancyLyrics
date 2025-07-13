8f32b6e330fef977470f84659b756103ecd1adef
Oliwier <fxgl1337@gmail.com>
2025-07-13 02:25:48 +0200

    Add lyrics fetching widget

--- fancyLyrics/Sources/fancyLyricsWidget.swift
+++ fancyLyrics/Sources/fancyLyricsWidget.swift
@@ -11,20 +11,39 @@ import AppKit
 import PockKit
 
 class fancyLyricsWidget: PKWidget {
-    
+
     static let identifier: String = "fixu.fancyLyrics"
     var customizationLabel: String = "fancyLyrics"
     var view: NSView!
-    
+
+    private let label = NSTextField(labelWithString: "…")
+    private var timer: Timer?
+
     required init() {
-        self.view = PKButton(title: "fancyLyrics", target: self)
+        label.font = NSFont.systemFont(ofSize: 13)
+        label.alignment = .center
+        label.lineBreakMode = .byWordWrapping
+        self.view = label
+
+        updateLyrics()
+        timer = Timer.scheduledTimer(withTimeInterval: 3.0, repeats: true) { [weak self] _ in
+            self?.updateLyrics()
+        }
     }
-    
-    @objc private func printMessage() {
-        let notification = NSUserNotification()
-        notification.title = "fancyLyrics"
-        notification.informativeText = "Widget copied to Pock!"
-        NSUserNotificationCenter.default.deliver(notification)
+
+    private func updateLyrics() {
+        guard let url = URL(string: "http://localhost:9863/2") else { return }
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, _, _ in
+            guard
+                let data = data,
+                let obj = try? JSONSerialization.jsonObject(with: data) as? [String: Any],
+                let text = obj["text"] as? String
+            else { return }
+            DispatchQueue.main.async {
+                self?.label.stringValue = text
+            }
+        }
+        task.resume()
     }
-    
+
 }
